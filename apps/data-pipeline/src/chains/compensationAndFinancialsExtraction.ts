f2edc4f8aad13b0f61653f2eed07b5d4d5ce747c
O.R <orezek@seznam.cz>
2025-10-23 14:23:23 +0200

    feat: finishing chain development

---
+++ apps/data-pipeline/src/chains/compensationAndFinancialsExtraction.ts
@@ -0,0 +1,79 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
+
+import { HumanMessage, AIMessage } from "@langchain/core/messages";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { compensationAndFinancialsSchema } from "../schemas/enrichedJobSchema/analyticalInsightsSchema/compensationAndFinancials/compensationAndFinancialsSchema.js";
+import {
+  humanMessageExample1,
+  humanMessageExample2,
+  humanMessageExample3,
+} from "../fewShotExamples/humanMessage/adExamples.js";
+import {
+  aiMessageExample1,
+  aiMessageExample2,
+  aiMessageExample3,
+} from "../fewShotExamples/aiMessage/compensationAndFinancials.js";
+
+import { companyAndTeamContextSystemMessage} from "../schemas/enrichedJobSchema/analyticalInsightsSchema/companyAndTeamContext/companyAndTeamContextSystemMessage.js";
+
+const GEM_MODELS_FLASH_LITE = "gemini-2.5-flash-lite";
+const GEM_MODELS_FLASH = "gemini-2.5-flash";
+const GEM_MODELS_PRO = "gemini-2.5-pro";
+
+const model = new ChatGoogleGenerativeAI({
+  model: GEM_MODELS_FLASH_LITE,
+  temperature: 0,
+});
+
+const systemMessage = PromptTemplate.fromTemplate(
+  companyAndTeamContextSystemMessage,
+);
+
+const humanMessage = PromptTemplate.fromTemplate("{placementText}");
+
+const examples = [
+  new HumanMessage(humanMessageExample1),
+  new AIMessage(JSON.stringify(aiMessageExample1)),
+  new HumanMessage(humanMessageExample2),
+  new AIMessage(JSON.stringify(aiMessageExample2)),
+  new HumanMessage(humanMessageExample3),
+  new AIMessage(JSON.stringify(aiMessageExample3)),
+];
+
+const extractCorePositionAndDetailsPrompt = new ChatPromptTemplate({
+  inputVariables: ["placementText", "examples"],
+  promptMessages: [
+    new SystemMessagePromptTemplate(systemMessage),
+    new MessagesPlaceholder("examples"),
+    new HumanMessagePromptTemplate(humanMessage),
+  ],
+});
+
+export async function extractCompensationAndFinancials(jobAd: string) {
+  try {
+    const extractCorePosition = await extractCorePositionAndDetailsPrompt
+      .pipe(
+        model.withStructuredOutput(compensationAndFinancialsSchema, {
+          name: "compensationAndFinancials",
+        }),
+      )
+      .invoke({ placementText: jobAd, examples: examples });
+    const validatedCorePositionDetails =
+      compensationAndFinancialsSchema.safeParse(extractCorePosition);
+    if (validatedCorePositionDetails.success) {
+      return validatedCorePositionDetails.data;
+    } else return null;
+  } catch (e) {
+    console.error("Failed to extract the CorePositionDetails.");
+    throw e;
+  }
+}
--- apps/data-pipeline/src/pipelines/dataEnrichmentPipeline.ts
+++ apps/data-pipeline/src/pipelines/dataEnrichmentPipeline.ts
@@ -5,32 +5,70 @@ import {extractTechnicalSkillsAndMethodologies} from "../chains/technicalSkillsA
 import {extractCorePositionAndDetails} from "../chains/corePositionDetailsExtraction.js";
 import {extractBenefitsAndPerks} from "../chains/benefitsAndPerksExtraction.js";
 import { extractCareerDevelopmentAndRecruitment } from "./../chains/careerDevelopmentAndRecruitmentExtraction.js"
+import type {AnyBulkWriteOperation} from "mongodb";
+import {extractCompanyAndTeamContext} from "../chains/companyAndTeamContextExtraction.js";
+import {extractCompensationAndFinancials} from "../chains/compensationAndFinancialsExtraction.js";
+import {extractContractualDetails} from "../chains/contractualDetailsExtraction.js";
+import {extractCulturalAndPsychologicalIndicators} from "../chains/culturalAndPsychologicalIndicators.js";
+import {extractLocationAndWorkModel} from "../chains/locationAndWorkModelExtraction.js";
+import {extractQualificationAndExperience} from "../chains/qualificationAndExperienceExtraction.js";
+import {extractWorkloadAndEnvironmentContext} from "../chains/workloadAndEnvironmentExtraction.js";
 dotenv.config();
 
 await connectToDb();
 const db = client.db('it-jobs');
 const enrichedJobsCollection = db.collection<EnrichedJobRecordsSchema>("enriched-job-records");
 
+const BATCH_SIZE = 3;
+let bulkWriteOperations: AnyBulkWriteOperation<EnrichedJobRecordsSchema>[] = [];
+
 async function processStagedJobs() {
   const enrichedJobs = enrichedJobsCollection.find({});
   for await (const enrichedJob of enrichedJobs) {
     // const extractedCorePosition = await extractCorePositionAndDetails(JSON.stringify(enrichedJob.originalAdData));
-    const extractedTechnicalSkills = await extractTechnicalSkillsAndMethodologies(JSON.stringify(enrichedJob.originalAdData));
-    const extractedBenefitsAndPerks = await extractBenefitsAndPerks(JSON.stringify(enrichedJob.originalAdData));
-    const extractedCareerDevelpmentAndRecruitement = await extractCareerDevelopmentAndRecruitment(JSON.stringify(enrichedJob.originalAdData));
-    await enrichedJobsCollection.updateOne(
-      {_id: enrichedJob._id},
-      {
-        $set: {
-          // Use "dot.notation.inside.quotes" to target the deep field
-          // "analyticalInsights.corePositionDetails": extractedCorePosition,
-          // "analyticalInsights.technicalSkillsAndMethodologies": extractedTechnicalSkills,
-          // "analyticalInsights.benefitsAndPerks": extractedBenefitsAndPerks,
-          "analyticalInsights.careerDevelopmentAndRecruitment": extractedCareerDevelpmentAndRecruitement,
+    // const extractedTechnicalSkills = await extractTechnicalSkillsAndMethodologies(JSON.stringify(enrichedJob.originalAdData));
+    // const extractedBenefitsAndPerks = await extractBenefitsAndPerks(JSON.stringify(enrichedJob.originalAdData));
+    // const extractedCareerDevelopmentAndRecruitment = await extractCareerDevelopmentAndRecruitment(JSON.stringify(enrichedJob.originalAdData));
+    const extractedCompanyAndTeamContext = await extractCompanyAndTeamContext(JSON.stringify(enrichedJob.originalAdData));
+    const extractedCompensationAndFinancials = await extractCompensationAndFinancials(JSON.stringify(enrichedJob.originalAdData));
+    const extractedContractualDetails = await extractContractualDetails(JSON.stringify(enrichedJob.originalAdData));
+    const extractedCulturalAndPsychologicalIndicators = await extractCulturalAndPsychologicalIndicators(JSON.stringify(enrichedJob.originalAdData));
+    const extractedLocationAndWorkModel = await extractLocationAndWorkModel(JSON.stringify(enrichedJob.originalAdData));
+    const extractedQualificationAndExperience = await  extractQualificationAndExperience(JSON.stringify(enrichedJob.originalAdData));
+    const extractedWorkloadAndEnvironmentContext = await extractWorkloadAndEnvironmentContext(JSON.stringify(enrichedJob.originalAdData));
+
+    bulkWriteOperations.push({
+      updateOne: {
+        filter: {_id: enrichedJob._id},
+        update: {
+          $set: {
+            // "analyticalInsights.corePositionDetails": extractedCorePosition,
+            // "analyticalInsights.technicalSkillsAndMethodologies": extractedTechnicalSkills,
+            // "analyticalInsights.benefitsAndPerks": extractedBenefitsAndPerks,
+            // "analyticalInsights.careerDevelopmentAndRecruitment": extractedCareerDevelopmentAndRecruitment,
+            "analyticalInsights.companyAndTeamContext": extractedCompanyAndTeamContext,
+            "analyticalInsights.compensationAndFinancials": extractedCompensationAndFinancials,
+            "analyticalInsights.contractulDetails": extractedContractualDetails,
+            "analyticalInsights.culturalAndPsychologicalIndicators": extractedCulturalAndPsychologicalIndicators,
+            "analyticalInsights.locationAndWorkModel": extractedLocationAndWorkModel,
+            "analyticalInsights.qualificationAndExperience": extractedQualificationAndExperience,
+            "analyticalInsights.WorkloadAndEnvironmentContext": extractedWorkloadAndEnvironmentContext
+          }
         }
       }
-    );
+    })
+
+    if (bulkWriteOperations.length >= BATCH_SIZE) {
+      console.log(`Writing batch of ${bulkWriteOperations.length} records.`);
+      await enrichedJobsCollection.bulkWrite(bulkWriteOperations);
+      bulkWriteOperations = [];
+    }
   }
+  if (bulkWriteOperations.length > 0) {
+    console.log(`Writing the final batch of ${bulkWriteOperations.length} records.`);
+    await enrichedJobsCollection.bulkWrite(bulkWriteOperations);
+  }
+
 }
 
 try {
